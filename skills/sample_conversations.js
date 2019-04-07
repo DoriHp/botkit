f5466c3a2c74f863a5b33c27cf3cea3ac69aa065
DoriHp <noobforever1502@gmail.com>
2019-04-07 15:02:04 +0700

    Success setting timeout for conversation

--- skills/sample_conversations.js
+++ skills/sample_conversations.js
@@ -16,17 +16,75 @@ module.exports = function(controller) {
 
         bot.startConversation(message, function(err, convo){
             convo.say('This is an example of using Spellcheck API and mongoStorage')
-            convo.ask('Input a sentences here:', function(response, convo){
-                //set option for request api
-                var options = {
-                    method: 'GET',
-                    uri: "https://montanaflynn-spellcheck.p.rapidapi.com/check/?text=" + response.text,
-                    headers: {
-                        'X-RapidAPI-Key': '7e6d1b4da1msh1138da086a26a0bp16ed03jsndad1f72ad6da'
+            convo.ask('Input a sentences here:', [
+                {
+                    pattern: 'timeout',
+                    callback: function(response, convo){
+                        convo.gotoThread('on_timeout')
                     }
-                };
+                },
+                {
+                    default: true,
+                    callback: function(response, convo){
+                        var result = ''
+                        var options = {
+                            method: 'GET',
+                            uri: "https://montanaflynn-spellcheck.p.rapidapi.com/check/?text=" + response.text,
+                            headers: {
+                                'X-RapidAPI-Key': '7e6d1b4da1msh1138da086a26a0bp16ed03jsndad1f72ad6da'
+                            }
+                        };
+                        function callback(err, response, body){
+                            result = JSON.parse(body)
+                            convo.ask('Did you mean "' + result.suggestion + '"?', [
+                                {
+                                    pattern:  bot.utterances.yes,
+                                    callback: function(response, convo) {
+                                        convo.gotoThread('yes_thread');
+                                    },
+                                },
+                                {
+                                    pattern:  bot.utterances.no,
+                                    callback: function(response, convo) {
+                                        convo.gotoThread('no_thread');
+                                    },
+                                },
+                            
+                                {
+                                    default: true,
+                                    callback: function(response, convo) {
+                                        convo.gotoThread('bad_response');
+                                    },
+                                }
+                            ])
 
-                convo.task.timeLimit = 3000
+                            convo.addMessage({
+                                    text: 'OK, I will save the correction.',
+                            },'yes_thread');
+
+                            // create a path for when a user says NO
+                            // mark the conversation as unsuccessful at the end
+                            convo.addMessage({
+                                text: 'OK, I will save the original.',
+                            },'no_thread');
+
+                            // create a path where neither option was matched
+                            // this message has an action field, which directs botkit to go back to the `default` thread after sending this message.
+                            convo.addMessage({
+                                text: 'Sorry I did not understand. Say `yes` or `no`',
+                                action: 'default',
+                            },'bad_response');
+
+                            convo.next()
+                            }
+                            request(options, callback)
+                        }
+                    }
+
+                ])
+                //set option for request api
+
+            convo.task.timeLimit = 10000
 
                 // convo.on('end',function(convo) {
 
@@ -38,63 +96,10 @@ module.exports = function(controller) {
 
                 // convo.setTimeout(3000)
 
-                convo.addMessage({
-                    text : 'Oh no, our time has expired!!!'
-                }, 'on_timeout')
-
-                function callback(err, response, body){
-                    let result = JSON.parse(body)
-                    if(result.original !== result.suggestion){
-                    //ask and create thread for the answer
-                    convo.ask('Did you mean "' + result.suggestion + '"?', [
-                        {
-                            pattern:  bot.utterances.yes,
-                            callback: function(response, convo) {
-                                convo.gotoThread('yes_thread');
-                            },
-                        },
-                        {
-                            pattern:  bot.utterances.no,
-                            callback: function(response, convo) {
-                                convo.gotoThread('no_thread');
-                            },
-                        },
-                    
-                        {
-                            default: true,
-                            callback: function(response, convo) {
-                                convo.gotoThread('bad_response');
-                            },
-                        }
-                    ])
-                    // create a path for when a user says YES
-                    convo.addMessage({
-                            text: 'OK, I will save the correction.',
-                    },'yes_thread');
-
-                    // create a path for when a user says NO
-                    // mark the conversation as unsuccessful at the end
-                    convo.addMessage({
-                        text: 'OK, I will save the original.',
-                    },'no_thread');
-
-                    // create a path where neither option was matched
-                    // this message has an action field, which directs botkit to go back to the `default` thread after sending this message.
-                    convo.addMessage({
-                        text: 'Sorry I did not understand. Say `yes` or `no`',
-                        action: 'default',
-                        },'bad_response');
-                    }else{
-                        convo.say("Message has been saved!")
-                    }
-                    convo.next()
-                }
-
-                request(options, callback)
-
-                    //Ask user to choose what is right
-                })
-            })
+            convo.addMessage({
+                text : 'Oh no, our time has expired!!!'
+            }, 'on_timeout')
+        })
     })
 
     controller.hears(['color'], 'message_received', function(bot, message) {
